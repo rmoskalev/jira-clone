da552d5730e4b82819309c038f1a8aadf54486d6
Ruslan <rmoskalevdev@gmail.com>
2024-10-19 15:31:31 +0300

    Fixed for build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div>
-      <Button variant={"destructive"} size={"lg"}>
-        Click me
-      </Button>
+      <Input></Input>
+      <Button>Primary</Button>
+      <Button variant={"secondary"}>Secondary</Button>
+      <Button variant={"destructive"}>Destructive</Button>
+      <Button variant={"ghost"}>ghost</Button>
+      <Button variant={"muted"}>muted</Button>
+      <Button variant={"outline"}>outline</Button>
+      <Button variant={"tertiary"}>tertiary</Button>
     </div>
   );
 }
