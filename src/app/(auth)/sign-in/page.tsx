2406daa1ec6501dffc0ee9de496915b046ee0bcc
Ruslan <rmoskalevdev@gmail.com>
2024-10-20 12:33:48 +0300

    Added sign-in and sign-up pages

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { SignInCard } from "@/features/auth/components";
+
+const SignInPage = () => {
+  return <SignInCard />;
+};
+
+export default SignInPage;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { SignUpCard } from "@/features/auth/components";
+
+const SignUpPage = () => {
+  return <SignUpCard />;
+};
+
+export default SignUpPage;
